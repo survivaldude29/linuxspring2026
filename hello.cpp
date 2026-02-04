73b131b7222a3a0e266220b44294ac745af8f2f2
survivaldude29 <neale.nicolas@gmail.com>
2026-02-04 20:11:16 +0000

    test 2-4-2026

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(int argc, char *argv[]) {
+if (argc > 1) {
+std::cout << "Hello, " << argv[1] << "!" << std::endl;
+} else {
+std::cout << "Hello, world!" << std::endl;
+}
+return 0;
+}
